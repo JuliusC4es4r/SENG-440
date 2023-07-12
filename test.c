ca331b57ed0277c796454bfede0e7c62030f3569
mohammadm10 <mohammadali_10@hotmail.com>
2023-07-12 11:38:39 -0700

    added scale factor and norm calcs

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #define MATRIX_SIZE 6
+#define MAX_SCALER 65536
 
 //the well-conditioned matrix to be inverted
 // line below for matlab debugging
@@ -112,15 +113,90 @@ void invertMatrix(float matrix[MATRIX_SIZE][MATRIX_SIZE], float inverse[MATRIX_S
     }
 }
 
+int scale_num_calculation(float matrix[MATRIX_SIZE][MATRIX_SIZE]){
+    printMatrix(matrix);
+    int scale_factor = 0;
+    int max = 0;
+
+    //Loop through matrix to find the maximum element
+    for(int i = 0; i < MATRIX_SIZE; i++){
+        for(int j = 0; j < MATRIX_SIZE; j++){
+            if (matrix[i][j] > max) max = matrix[i][j];
+        }
+    }
+    printf("max: %d\n", max);
+    if(max > 0) scale_factor = MAX_SCALER/max;
+
+    return scale_factor;
+}
+
+int scale_matrix(float inverted_matrix[MATRIX_SIZE][MATRIX_SIZE], int scale_factor){
+    //Scale each element in the matrix
+    for(int i = 0; i < MATRIX_SIZE; i++){
+        for(int j = 0; j < MATRIX_SIZE; j++){
+            inverted_matrix[i][j]*=scale_factor;
+        }
+    }
+}
+
+int approximate_norm(float matrix[MATRIX_SIZE][MATRIX_SIZE]){
+    //Goal: estimate the condition number without calculating the norm
+    //Approximate the matrix norm by find the maximum absolute row sum
+
+    printMatrix(matrix);
+
+    int norm = 0;
+    for(int i = 0; i < MATRIX_SIZE; i++){
+        int row_sum = 0;
+        for(int j = 0; j < MATRIX_SIZE; j++){
+            if(matrix[i][j] < 0){
+                //Multiply index by -1 if negative
+                row_sum+=(matrix[i][j]*(-1));
+            }else{
+                row_sum+=matrix[i][j];
+            }
+        }
+        if(row_sum > norm) norm = row_sum;
+    }
+    return norm;
+}
+
+int compute_condition_num(float matrix[MATRIX_SIZE][MATRIX_SIZE]){
+    //Goal: estimate the condition number without calculating the norm
+    //Approximate the matrix norm by find the maximum absolute row sum
+
+    int norm = approximate_norm(matrix);
+    printf("Norm is:%d\n", norm);
+
+    //perturb the matrix by 25
+    float perturbed[MATRIX_SIZE][MATRIX_SIZE];
+    for(int i = 0; i < MATRIX_SIZE; i++){
+        for(int j = 0; j < MATRIX_SIZE; j++){
+            perturbed[i][j] = matrix[i][j]+25;
+        }
+    }
+    int perturbed_norm = approximate_norm(perturbed);
+    printf("Per norm:%d\n", perturbed_norm);
+}
+
 int main() {
 
-    printf("Original Matrix:\n");
-    printMatrix(well_conditioned_matrix);
+    compute_condition_num(well_conditioned_matrix);
 
-    invertMatrix(well_conditioned_matrix, well_conditioned_inversion_result);
+    // printf("Original Matrix:\n");
+    // printMatrix(well_conditioned_matrix);
 
-    printf("Inverted Matrix:\n");
-    printMatrix(well_conditioned_inversion_result);
+    // invertMatrix(well_conditioned_matrix, well_conditioned_inversion_result);
+
+    // printf("Inverted Matrix:\n");
+    // printMatrix(well_conditioned_inversion_result);
+
+    // int scale = scale_num_calculation(well_conditioned_matrix);
+    // printf("Scale Number: %d\n", scale);
+
+    // scale_matrix(well_conditioned_inversion_result, scale);
+    // printf("Scaled inverse matrix:\n");
+    // printMatrix(well_conditioned_inversion_result);
 
     return 0;
 }
