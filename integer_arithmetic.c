a4d5b79a8d612b992b6f7fc3ca67373c86600e58
mohammadm10 <mohammadali_10@hotmail.com>
2023-07-19 15:22:35 -0700

    commit

--- integer_arithmetic.c
+++ integer_arithmetic.c
@@ -1,99 +1,162 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<math.h>
 
-#define N 6
+#define MATRIX_SIZE 6
+#define SCALE_FACTOR (1 << 16) // 2^16
 
-void printMatrix(int matrix[N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d ", matrix[i][j]);
+int matrix[MATRIX_SIZE][MATRIX_SIZE];
+int inverse[MATRIX_SIZE][MATRIX_SIZE];
+int temp;
+
+int matrix[MATRIX_SIZE][MATRIX_SIZE] = 
+{
+    {5, 2, 1, 1, 2, 1},
+    {5, 1, 7, 1, 1, 3},
+    {7, 1, 7, 9, 1, 2},
+    {1, 1, 5, 3, 3, 5},
+    {1, 2, 5, 2, 2, 1},
+    {1, 9, 0, 5, 1, 7}
+};
+
+int inverse[MATRIX_SIZE][MATRIX_SIZE] = 
+{
+    {1, 0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 1},
+};
+
+void printMatrix(int matrix[MATRIX_SIZE][MATRIX_SIZE]){
+    int i,j;
+    for(i=0;i<MATRIX_SIZE;i++){
+        for(j=0;j<MATRIX_SIZE;j++){
+            printf("%d ",matrix[i][j]);
         }
         printf("\n");
     }
 }
 
-void integerMatrixInverse(int matrix[N][N]) {
-    int scaled_matrix[N][N];
-    int inv_scaled_matrix[N][N];
-    int augmented_matrix[N][2 * N];
-    int scale_factor = 0;
+void gaussJordan(){
+    int i, j, k;
+    int zero_diagonal = 0; // Initialize flag to false
 
-    // Compute the scale factor
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (abs(matrix[i][j]) > scale_factor) {
-                scale_factor = abs(matrix[i][j]);
-            }
-        }
-    }
+    printf("\nMatrix before\n");
+    printMatrix(matrix);
 
-    // Scale the matrix
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            scaled_matrix[i][j] = (matrix[i][j] * (1 << 32)) / scale_factor;
-        }
-    }
-
-    // Perform Gauss-Jordan elimination
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            augmented_matrix[i][j] = scaled_matrix[i][j];
-            augmented_matrix[i][j + N] = (i == j) ? 1 : 0;
-        }
-    }
-
-    for (int k = 0; k < N; k++) {
-        int pivot = augmented_matrix[k][k];
-
-        // Scale the pivot row
-        int scale = (1 << 32) / pivot;
-        for (int j = 0; j < 2 * N; j++) {
-            augmented_matrix[k][j] = ((int64_t) augmented_matrix[k][j] << 32) / scale;
-        }
-
-        // Eliminate other rows
-        for (int i = 0; i < N; i++) {
-            if (i != k) {
-                int factor = ((int64_t) augmented_matrix[i][k] << 32) / pivot;
-                for (int j = 0; j < 2 * N; j++) {
-                    augmented_matrix[i][j] ^= ((int64_t) augmented_matrix[k][j] << 32) / factor;
+    for (i = 0; i < MATRIX_SIZE; i++) {
+        for (j = 0; j < MATRIX_SIZE; j++) {
+            if (i != j) {
+                printf("\n%d\n", matrix[i][i]);
+                if (matrix[i][i] == 0) {
+                    zero_diagonal = 1; // Set flag to true
+                    break; // Exit inner loop if diagonal element is zero
+                }
+                temp = matrix[j][i] / matrix[i][i];
+                for (k = 0; k < MATRIX_SIZE; k++) {
+                    matrix[j][k] -= matrix[i][k] * temp;
+                    inverse[j][k] -= inverse[i][k] * temp;
                 }
             }
         }
     }
+    // for(i = 0; i < MATRIX_SIZE; i++){
+    //     int pivot = matrix[i][i];
+    //     for(j = 0; j < MATRIX_SIZE; j++){
+    //         int multiple = matrix[i][j] /= pivot;
+    //         if(i == j){
+    //             matrix[i][j] /= matrix[i][i];
+    //         }else{
+    //             int temp = multiple*matrix[i][j];
+    //             matrix[i][j] = matrix[i][j] - 
+    //         }
+    //     }
+    // }
+    printf("\nMatrix after\n");
 
-    // Extract the inverted matrix
-    for (int i = 0; i < N; i++) {
-        for (int j = N; j < 2 * N; j++) {
-            inv_scaled_matrix[i][j - N] = augmented_matrix[i][j];
+    printMatrix(matrix);
+    for(i=0;i<MATRIX_SIZE;i++){
+        temp=matrix[i][i];
+        if(temp == 0){
+            return;
         }
-    }
-
-    // Rescale the inverted matrix
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            matrix[i][j] = (inv_scaled_matrix[i][j] * scale_factor) / (1 << 32);
+        for(j=0;j<MATRIX_SIZE;j++){
+            matrix[i][j]=matrix[i][j]/temp;
+            inverse[i][j]=(inverse[i][j]*SCALE_FACTOR)/temp;
         }
     }
 }
 
-int main() {
-    int matrix[N][N] = {
-        {1, 2, 1, 1, 2, 1},
-        {5, 2, 7, 1, 1, 3},
-        {7, 1, 1, 4, 1, 2},
-        {1, 1, 5, 2, 3, 7},
-        {1, 2, 5, 2, 2, 1},
-        {1, 2, 1, 1, 1, 2}
-    };
+int scale_factor_calculation(int matrix[MATRIX_SIZE][MATRIX_SIZE]){
+    int scale_factor = 0;
+    int max = 0;
 
-    printf("Original Matrix:\n");
+    //Loop through matrix to find the maximum element
+    for(int i = 0; i < MATRIX_SIZE; i++){
+        for(int j = 0; j < MATRIX_SIZE; j++){
+            if (matrix[i][j] > max) max = matrix[i][j];
+        }
+    }
+    if(max > 0) scale_factor = SCALE_FACTOR/max;
+
+    return scale_factor;
+}
+
+void scale_matrix(int matrix[MATRIX_SIZE][MATRIX_SIZE], int scale_factor){
+    //Scale each element in the matrix
+    for(int i = 0; i < MATRIX_SIZE; i++){
+        for(int j = 0; j < MATRIX_SIZE; j++){
+            matrix[i][j] *= scale_factor;
+        }
+    }
+}
+
+int approximate_norm(int matrix[MATRIX_SIZE][MATRIX_SIZE]){
+    //Goal: estimate the condition number without calculating the norm
+    //Approximate the matrix norm by finding the maximum absolute row sum
+
+    int norm = 0;
+    for(int i = 0; i < MATRIX_SIZE; i++){
+        int row_sum = 0;
+        for(int j = 0; j < MATRIX_SIZE; j++){
+            if(matrix[i][j] < 0){
+                //Multiply index by -1 if negative
+                row_sum += (matrix[i][j] * (-1));
+            }else{
+                row_sum += matrix[i][j];
+            }
+        }
+        if(row_sum > norm) norm = row_sum;
+    }
+    return norm;
+}
+
+int compute_condition_num(int matrix[MATRIX_SIZE][MATRIX_SIZE], int inverse[MATRIX_SIZE][MATRIX_SIZE], int scale_factor){
+    //Goal: estimate the condition number without calculating the norm
+    //Approximate the matrix norm by finding the maximum absolute row sum
+    int norm = approximate_norm(matrix);
+    printf("Norm is:%d\n", norm);
+
+    gaussJordan();
+
+    int inverse_norm = approximate_norm(inverse);
+    printf("Inverse norm is:%d\n", inverse_norm);
+
+    int condition_num = norm * inverse_norm;
+    
+    return condition_num;
+}
+
+int main(){
     printMatrix(matrix);
-
-    integerMatrixInverse(matrix);
-
-    printf("Inverted Matrix:\n");
-    printMatrix(matrix);
-
+    int scale_factor = scale_factor_calculation(matrix);
+    printf("Scale factor: %d\n", scale_factor);
+    scale_matrix(matrix, scale_factor);
+    int condition_number = compute_condition_num(matrix, inverse, scale_factor);
+    printf("Condition Number: %d\n", condition_number);
+    
+    printf("\nInverse Matrix is:\n");
+    printMatrix(inverse);
     return 0;
 }
