4615425f2cddb8feac35dbb56ad34162c2310c60
Miguel_Manguerra <migoymanguerra@gmail.com>
2023-07-31 01:16:41 -0700

    adding hardware mult assembly

---
+++ hardware_multiplier.s
@@ -0,0 +1,29 @@
+	.file	"hardware_multiplier.c"
+	.text
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB13:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	call	___main
+/APP
+ # 8 ".\hardware_multiplier.c" 1
+	hardwareMultiplier
+ # 0 "" 2
+/NO_APP
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE13:
+	.ident	"GCC: (Raspbian 10.2.1-6+rpi1) 10.2.1 20210110"
+	.section	.note.GNU-stack,"",%progbits
