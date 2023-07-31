728dd77bd19821034814199ae372d5ea25dbcb21
Miguel_Manguerra <migoymanguerra@gmail.com>
2023-07-30 22:51:20 -0700

    playing around with hardware mult

--- hardware_multiplier.c
+++ hardware_multiplier.c
@@ -3,20 +3,9 @@
 #define MatrixSize 6
 #define MaxScale 65536 // 2^16
 
-int hardwareMultiplier(){
 
+int main() {
+    __asm__ ( "hardwareMultiplier");
+
+    return 0;
 }
-
-void scaleMatrix(int matrix[MatrixSize][MatrixSize], int scale_factor){
-    //Scale each element in the matrix
-    for(int i = 0; i < MatrixSize; i+=2){
-        for(int j = 0; j < MatrixSize; j+=2){
-            matrix[i][j] = hardwareMultiplier(scale_factor,  matrix[i][j]);
-            matrix[i][j+1] = hardwareMultiplier(scale_factor, matrix[i][j+1]);
-
-            matrix[i+1][j] = hardwareMultiplier(scale_factor, matrix[i+1][j]);
-            matrix[i+1][j+1] = hardwareMultiplier(scale_factor, matrix[i+1][j+1]);
-        }
-    }
-}
-
